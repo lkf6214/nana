7cc64c7bc9b2496cd963dacc3018804df284e8dd
lkf6214 <lkf6214@naver.com>
2023-10-02 21:19:00 +0900

    fix: 경로 변경

--- src/pages/Neuronet.jsx
+++ src/pages/Neuronet.jsx
@@ -88,14 +88,14 @@ export default function Neuronet() {
         "Personal Work : Fashion Character Drawing Style Ver.03 🧡💛",
     },
     {
-      src: "https://lkf6214.github.io/nana/img/Neuronet/15.MP4",
+      src: process.env.PUBLIC_URL + "/img/Neuronet/15.MP4",
       // src: process.env.PUBLIC_URL + "/img/Neuronet/15.mp4",
       alt: "Neuronet 15",
       description: "Personal Work : (((o(・ω・o) (o・ω・)o)))",
     },
     {
       // src: process.env.PUBLIC_URL + "/img/Neuronet/16.mp4",
-      src: "https://lkf6214.github.io/nana/img/Neuronet/16.MP4",
+      src: process.env.PUBLIC_URL + "/img/Neuronet/16.MP4",
       alt: "Neuronet 16",
       description:
         "Hyundai Motors Digital Art Contest : Freedom Of Movement 💫",
